3034d00f20f4d1de43f8a3bb4742ef0d707dbdb0
ezequias <ezequiasantunes18@gmail.com>
2021-08-02 12:45:51 -0300

    Quarto Commit

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
 
-/* //Get Heroku ClearDB connection information
+//Get Heroku ClearDB connection information
 $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $cleardb_server = $cleardb_url["host"];
 $cleardb_username = $cleardb_url["user"];
@@ -12,8 +12,8 @@ $query_builder = TRUE;
 // Connect to DB
 $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
- */
-$servername = "us-cdbr-east-04.cleardb.com";
+
+/* $servername = "us-cdbr-east-04.cleardb.com";
 $username = "b61712efcdb316";
 $password = "b599cf57";
 $database = "rankingre9";
@@ -23,6 +23,6 @@ $conn = new mysqli($servername, $username, $password,$database);
 
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
-}
+} */
 
 ?>
