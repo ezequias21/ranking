329a3d7ca8faa18129b7a9eed896d0d0e417fdcc
ezequias <ezequiasantunes18@gmail.com>
2021-08-02 12:28:53 -0300

    Terceiro Commit

--- config.php
+++ config.php
@@ -1,8 +1,21 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
+
+/* //Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+ */
+$servername = "us-cdbr-east-04.cleardb.com";
+$username = "b61712efcdb316";
+$password = "b599cf57";
 $database = "rankingre9";
 
 
